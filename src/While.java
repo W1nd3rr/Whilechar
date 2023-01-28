7835cc3ff4112c21082f82dacea40080df09ac76
Pavilion <Pavilion@WinsLee>
2023-01-28 13:23:25 +0700

    first commit

---
+++ src/While.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+
+public class While {
+
+    public static void main(String[] args) {
+        char huruf = 'z';
+        while (huruf >= 'a')
+        {
+            System.out.println(huruf);
+            huruf--;
+        }
+    }
+    
+}
